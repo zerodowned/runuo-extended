6cb2fc76cdbd5d47a61b82ca22a107e08456a8ae
ericmas001 <ericmas001@ec3d1d00-9d98-d657-809b-6ef90030b600>
2010-09-02 18:00:11 +0000

    Change Server Name

--- RunUOExtended/trunk/RunUOExtended/Scripts/Misc/ServerList.cs
+++ RunUOExtended/trunk/RunUOExtended/Scripts/Misc/ServerList.cs
@@ -39,7 +39,7 @@ namespace Server.Misc
 		 */
 
 		public static readonly string Address = null;
-		public static readonly string ServerName = "RunUO TC";
+		public static readonly string ServerName = "RunUO 2.0 Final Extended";
 
 		public static readonly bool AutoDetect = true;
 
